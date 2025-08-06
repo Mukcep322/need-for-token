8c11d9aa65cb1ea3de8f23858805e68559929b61
Mukcep322 <antongolosov48@gmail.com>
2025-08-06 20:44:42 +0300

    redesign...

--- f/src/main.jsx
+++ f/src/main.jsx
@@ -3,11 +3,6 @@ import {createRoot} from "react-dom/client"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import App from "./pages/MainPage.jsx"
-import Market from "./pages/Market.jsx"
-import Header from "./components/Header.jsx"
-import Leaderboard from "./pages/Leaderboard.jsx"
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx"
 
 createRoot(document.getElementById("root")).render(
   <div className="relative">
@@ -15,11 +10,11 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/Market" element={<Market />} />
+        {/* <Route path="/Market" element={<Market />} />
         <Route path="/Leaderboard" element={<Leaderboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="*" element={<div>404</div>} />
+        <Route path="*" element={<div>404</div>} /> */}
       </Routes>
     </BrowserRouter>
   </div>
--- f/src/pages/MainPage.jsx
+++ f/src/pages/MainPage.jsx
@@ -1,49 +1,7 @@
 import "../Main.css"
-import FaultyTerminal from "../ReactBits/Backgrounds/FaultyTerminal/FaultyTerminal.jsx"
 
 function App() {
-  return (
-    <>
-      <div className="w-full h-[100vh] relative">
-        <FaultyTerminal
-          scale={2.5}
-          gridMul={[2, 1]}
-          digitSize={1.2}
-          timeScale={0.5}
-          pause={false}
-          scanlineIntensity={1}
-          glitchAmount={1}
-          flickerAmount={1}
-          noiseAmp={1}
-          chromaticAberration={0}
-          dither={1}
-          curvature={0.1}
-          tint="#32502D"
-          mouseReact={true}
-          mouseStrength={0.5}
-          pageLoadAnimation={false}
-          brightness={1}
-          className={"absolute"}
-        />
-        <img
-          src="./imgInMain.png"
-          className="absolute bottom-0 left-0 w-[30vw]"
-        />
-        <div className="flex flex-col absolute right-[240px] bottom-[60px] z-10 gap-[150px] items-end">
-          <span className="text-white text-5xl font-[Jost] font-light text-right">
-            Lorem ipsum dolor <br /> sit amet consectetur.Lorem <br />
-            ipsum dolor sit amet <br />
-            consectetur.
-          </span>
-          <a
-            href="/auth/register"
-            className="bg-[#83EA05] text-black font-[Unbounded] w-fit rounded-full text-3xl px-10 py-5">
-            Start Trading
-          </a>
-        </div>
-      </div>
-    </>
-  )
+  return <div className="main-container">123</div>
 }
 
 export default App
