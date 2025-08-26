5cee9cfd263e1dd5238d930074cebf32916d943d
Mukcep322 <antongolosov48@gmail.com>
2025-08-26 17:01:57 +0300

    better header

--- f/src/components/shared/Header.jsx
+++ f/src/components/shared/Header.jsx
@@ -48,22 +48,24 @@ export function Header() {
 
           <div className="flex gap-3 items-center">
             {isAuth ? (
-              <Link to="/profile">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-user-icon lucide-user">
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
-                </svg>
-              </Link>
+              <div className="w-[78px] flex justify-end ">
+                <Link to="/profile">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-user-icon lucide-user">
+                    <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                    <circle cx="12" cy="7" r="4" />
+                  </svg>
+                </Link>
+              </div>
             ) : (
               <Button Link="/login" Dark={true}>
                 Login
@@ -77,60 +79,3 @@ export function Header() {
     </motion.header>
   )
 }
-
-// export function Header() {
-//   const [isHover, setIsHovered] = useState(false)
-
-//   function setIsHover(value) {
-//     setIsHovered(value)
-//   }
-
-//   return (
-//     <motion.header
-//       className={`flex gap-20 items-center  pl-4 pr-2.5 py-5 rounded-full glass-effect justify-between h-[65px] relative transition-all duration-500 width-[360px] z-20`}
-//       whileHover={{
-//         width: 800,
-//         transition: {
-//           type: "tween",
-//           ease: "linear",
-//           duration: 0.3
-//         }
-//       }}
-//       onMouseEnter={() => setIsHover(true)}
-//       onMouseLeave={() => setIsHover(false)}>
-//       {/* logo */}
-//       <img src="/Logo.png" alt="logo" className="w-[200px]" />
-//       {/* hover me */}
-//       {isHover ? (
-//         ""
-//       ) : (
-//         <span
-//           className={`text-[#888888] font-[Jost] text-lg cursor-pointer hover:text-gray-300 pr-2 ${
-//             isHover ? "opacity-0" : "opacity-100"
-//           } transition-opacity duration-1000`}>
-//           Hover me!
-//         </span>
-//       )}
-
-//       {/* hrefs */}
-//       {!isHover ? (
-//         ""
-//       ) : (
-//         <>
-//           <div
-//             className={`flex gap-5 items-center font-[Jost] text-lg ${
-//               isHover ? "opacity-100" : "opacity-0"
-//             } transition-opacity duration-5000`}>
-//             <a href="/">Home</a>
-//             <a href="/Market">Market</a>
-//             <a href="/Leaderboard">Leaderboard</a>
-//           </div>
-//           {/* btn */}
-//           <Button Link={"/login"} Dark={true}>
-//             Login
-//           </Button>
-//         </>
-//       )}
-//     </motion.header>
-//   )
-// }
